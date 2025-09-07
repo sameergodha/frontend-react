fa14fb7f49de1d2fbe37ef2099c71ff1cbcfe253
sameergodha <sameer.godha.pm@gmail.com>
2025-09-07 21:22:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
+import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+import Navbar from './components/Navbar'
+import './css/App.css'
+import { MovieProvider } from './contexts/MovieContext' 
+
+function App() {
+  return (
+    <MovieProvider>
+      <Navbar/>
+    <main className = "main-content">
+      <Routes>
+        <Route path = "/home" element = {<Home/>}/>
+        <Route path = "/favorites" element = {<Favorites/>}/>
+      </Routes>
+    </main>
+    </MovieProvider>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,70 @@
+import MovieCard from "../components/MovieCard"
+import { useState, useEffect } from "react";
+import '../css/Home.css'
+import { searchMovies, getPopularMovies } from "../services/api";
+
+function Home() {
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    {/* this should not be called every time, we render the component */}
+    const[movies, setMovies] = useState([])
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+            } catch (err) {
+                console.log(err);
+                setError("Failed to load movies");
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+         loadPopularMovies()
+    }, [])
+
+    const HandleSearch =async (e) => {
+        e.preventDefault();
+        if (searchQuery.trim() === "") return     
+        if (loading) return
+        setLoading(true);
+        try{
+            const searchResults = await searchMovies(searchQuery);
+            setMovies(searchResults);
+            setError(null);
+        }catch(err){
+            console.log(err);
+            setError("Failed to search movie");
+        }finally{
+            setLoading(false);      
+        }
+    };
+    return <> 
+    <div className="home">
+        <form onSubmit={HandleSearch} className="search-form">
+            <input 
+            type="text" 
+            placeholder="Search Movies" 
+            className="search-input" 
+            value = {searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">Search</button>
+        </form>
+        {error && <div className="error-message">{error}</div>} 
+        {loading ? <div> Loading... </div> : <div className="movies-grid">
+            {movies.map(
+                (movie) => (
+                    <MovieCard movie = {movie} key = {movie.id}/>
+                )
+            )}
+        </div>}
+        
+    </div>
+    </>
+}
+export default Home
